c1fbeae8a2c13df85ad290ec7d3989f3ff7b2c62
Jacek <jacek@gooddata.com>
2022-08-04 17:06:10 +0200

    Update data transformations to absorb new input model
    Single JSON item column per table
    
    + Split env vars for INPUT and OUTPUT schemas

--- data_transformation/models/pull_requests/pull_requests.sql
+++ data_transformation/models/pull_requests/pull_requests.sql
@@ -1,29 +1,39 @@
 with pull_requests as (
-    select * from cicd_input_stage.pull_requests 
+    select to_json("item") as item_json from cicd_input_stage.pulls
 ),
 
 users as (
     select * from {{ ref('users') }}
 ),
 
-extract_from_pull_requests as (
-    select id as pull_request_id, CAST(json_extract_path_text(to_json("user"), 'id') as INT) as user_id, created_at, merged_at from pull_requests
+extract_pulls as (
+    select
+      CAST(json_extract_path_text(item_json, 'id') as INT) as pull_request_id,
+      CAST(json_extract_path_text(item_json, 'html_url') as TEXT) as pull_request_url,
+      CAST(json_extract_path_text(item_json, 'user', 'id') as INT) as user_id,
+      to_timestamp(
+        CAST(json_extract_path_text(item_json, 'created_at') as TEXT),
+        '{{ var("github_date_format") }}'
+      ) at time zone '{{ var("timezone") }}' as created_at,
+      to_timestamp(
+        CAST(json_extract_path_text(item_json, 'merged_at') as TEXT),
+        '{{ var("github_date_format") }}'
+      ) at time zone '{{ var("timezone") }}' as merged_at
+    from pull_requests
 ),
 
 final as (
-    select 
-    
-    extract_from_pull_requests.pull_request_id, 
-    extract_from_pull_requests.user_id, 
-    users.login, 
-    extract_from_pull_requests.created_at, 
-    extract_from_pull_requests.merged_at 
-
-    from extract_from_pull_requests
- 
-    left join users on extract_from_pull_requests.user_id = users.user_id
-
-    where extract_from_pull_requests.user_id is not null and users.login is not null and extract_from_pull_requests.merged_at is not null
+    select
+      extract_pulls.pull_request_id,
+      extract_pulls.pull_request_url,
+      extract_pulls.user_id,
+      extract_pulls.created_at,
+      extract_pulls.merged_at,
+      (extract(epoch from extract_pulls.merged_at) - extract(epoch from extract_pulls.created_at)) / 3600 as hours_to_solve
+    from extract_pulls
+    -- Filter out commits without responsible user (data cleansing)
+    join users on extract_pulls.user_id = users.user_id
+    where extract_pulls.user_id is not null and users.login is not null and extract_pulls.merged_at is not null
 )
 
 select * from final
