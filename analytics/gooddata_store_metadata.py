4bb4131ff74964cdcede8e08c0de26384b6626e5
Jacek <jacek@gooddata.com>
2022-08-31 11:37:25 +0200

    Extend configuration by data sources
    Enable setting different name for each data source
    In the future, we can move more data source properties there, but it must be aligned with dbt!
    Idea: shared config script setting ENV variables for dbt, or providing python config class for GoodData tools

--- analytics/gooddata_store_metadata.py
+++ analytics/gooddata_store_metadata.py
@@ -1,11 +1,11 @@
 import os
 from gooddata_sdk import GoodDataSdk
-from args import parse_arguments
+from args import parse_arguments_ws
 from config import Config
 
 host = os.environ["GOODDATA_HOST"]
 token = os.environ["GOODDATA_TOKEN"]
-args = parse_arguments()
+args = parse_arguments_ws("Get metadata from GoodData and store them to disk")
 config = Config(args.config)
 workspace = config.get_workspace(args.workspace_id)
 
