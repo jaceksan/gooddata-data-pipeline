efe5a9ddd226ccd8f7e5f2dd59f96ac619ad69f1
Jacek <jacek@gooddata.com>
2022-12-08 14:41:18 +0100

    Fix bootstrap of DB schemas

--- fixtures/postgres/00_create_db_schema.sql
+++ fixtures/postgres/00_create_db_schema.sql
@@ -1,15 +1,2 @@
-create database cicd_dev;
-
-\c cicd_dev
-
-create schema cicd_input_stage;
-create schema cicd_output_stage;
-
-create user cicd password 'cicd123';
-grant all on schema cicd_input_stage to cicd;
-grant all on schema cicd_output_stage to cicd;
-grant all on all tables in schema cicd_input_stage to cicd;
-grant all on all tables in schema cicd_output_stage to cicd;
-alter default privileges in schema cicd_input_stage grant all on tables to cicd;
-
-grant create on database cicd_dev to cicd;
+create schema if not exists cicd_input_stage;
+create schema if not exists cicd_output_stage;
