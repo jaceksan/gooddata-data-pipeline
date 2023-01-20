43487d263a02f316e300631628efebe4f81f9901
Jacek <jacek@gooddata.com>
2023-01-20 10:42:28 +0100

    Incremental dbt transformation
    following the incremental approach introduced recently with Meltano

--- src/models/commits/commits_extract_json.sql
+++ src/models/commits/commits_extract_json.sql
@@ -3,31 +3,48 @@
   indexes=[
     {'columns': ['commit_id'], 'unique': true},
     {'columns': ['user_url'], 'unique': false}
-  ]
+  ],
+  materialized='incremental',
+  unique_key='commit_id',
+  incremental_strategy='delete+insert'
 ) }}
 
 -- Helper step, materialize extracted JSON fields first and then JOIN it with other tables
+-- Incremental mode
 
-with commits_extracted as (
-    select
-      commit_timestamp,
-      sha,
-      html_url,
-      repo_id,
-      to_json(author) as author_json,
-      to_json(commit) as commit_json
-    from {{ var("input_schema") }}.commits
+with using_clause as (
+  select
+    sha as commit_id,
+    html_url as commit_url,
+    CAST(json_extract_path_text(to_json(commit), 'comment_count') as INT) as comment_count,
+    commit_timestamp as created_at,
+    CAST(json_extract_path_text(to_json(author), 'url') as TEXT) as user_url,
+    repo_id
+  from {{ var("input_schema") }}.commits
+  {% if is_incremental() %}
+    where commit_timestamp > ( select max(created_at) from {{ this }} )
+  {% endif %}
+),
+
+updates as (
+  select *
+  from using_clause
+  {% if is_incremental() %}
+    where commit_id in ( select commit_id from {{ this }} )
+  {% endif %}
+),
+
+inserts as (
+  select *
+  from using_clause
+  {% if is_incremental() %}
+    where commit_id not in ( select commit_id from {{ this }} )
+  {% endif %}
 ),
 
 final as (
-    select
-      sha as commit_id,
-      html_url as commit_url,
-      CAST(json_extract_path_text(commit_json, 'comment_count') as INT) as comment_count,
-      commit_timestamp as created_at,
-      CAST(json_extract_path_text(author_json, 'url') as TEXT) as user_url,
-      repo_id
-    from commits_extracted
+  select * from inserts
+  union all select * from updates
 )
 
 select * from final
