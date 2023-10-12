b6899a614c1121c1fa9b6d53b4f4b3f81cfbbfd2
Jacek <jacek@gooddata.com>
2023-10-12 14:26:53 +0200

    Fix home_rental dataset
    Use valid data types (INT instead of DECIMAL)
    Populate number_of_rooms as both attribute and fact
    Override INT columns to facts where necessary

--- data_pipeline/models/data_science/home_rentals/home_rentals.sql
+++ data_pipeline/models/data_science/home_rentals/home_rentals.sql
@@ -2,10 +2,11 @@ with final as (
   select
     location,
     neighborhood,
-    CAST(number_of_rooms AS DECIMAL(15, 2)) as number_of_rooms,
-    CAST(number_of_bathrooms AS DECIMAL(15, 2)) as number_of_bathrooms,
+    CAST(number_of_rooms AS INT) as number_of_rooms,
+    CAST(number_of_rooms AS INT) as number_of_rooms_fact,
+    CAST(number_of_bathrooms AS INT) as number_of_bathrooms,
     CAST(replace(sqft, ',', '.') AS DECIMAL(15, 2)) as square_feets,
-    CAST(days_on_market AS DECIMAL(15, 2)) as days_on_market,
+    CAST(days_on_market AS INT) as days_on_market,
     CAST(initial_price AS DECIMAL(15, 2)) as initial_price,
     CAST(rental_price AS DECIMAL(15, 2)) as rental_price
   from {{ var("input_schema_data_science") }}.home_rentals
