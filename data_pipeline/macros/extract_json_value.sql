555b7d343198fa28c6224c2dd3ac9009e3ac8402
Jacek <jacek@gooddata.com>
2024-02-14 15:57:38 +0100

    Fix extract_json dbt macro for DuckDB
    Extracted JSON path must be prefixed by $.

--- data_pipeline/macros/extract_json_value.sql
+++ data_pipeline/macros/extract_json_value.sql
@@ -1,19 +1,23 @@
 --
-{% macro extract_json_value(json_column_name, field_name, target_column_name, data_type) -%}
+{% macro extract_json_value(json_column_name, json_path, target_column_name, data_type) -%}
 
     {%- set db_entity_name = get_db_entity_name(json_column_name) -%}
 
     {%- if target.type == "snowflake" -%}
 
-        CAST(json_extract_path_text(to_json(parse_json("{{ db_entity_name }}")), '{{ field_name }}') AS {{ data_type }}) AS {{ target_column_name }}
+        CAST(json_extract_path_text(to_json(parse_json("{{ db_entity_name }}")), '{{ json_path }}') AS {{ data_type }}) AS {{ target_column_name }}
 
     {%- elif target.type == "vertica" -%}
 
-        CAST((public.MapJSONExtractor("{{ db_entity_name }}"))['{{ field_name }}'] AS {{ data_type }}) AS {{ target_column_name }}
+        CAST((public.MapJSONExtractor("{{ db_entity_name }}"))['{{ json_path }}'] AS {{ data_type }}) AS {{ target_column_name }}
 
-    {%- elif target.type in ("postgres", "duckdb") -%}
+    {%- elif target.type in ("postgres") -%}
 
-        CAST(json_extract_path_text("{{ db_entity_name }}"::JSON, '{{ field_name }}') AS {{ data_type }}) AS {{ target_column_name }}
+        CAST(json_extract_path_text("{{ db_entity_name }}"::JSON, '{{ json_path }}') AS {{ data_type }}) AS {{ target_column_name }}
+
+    {%- elif target.type in ("duckdb") -%}
+
+        CAST(json_extract_path_text("{{ db_entity_name }}"::JSON, '$.{{ json_path }}') AS {{ data_type }}) AS {{ target_column_name }}
 
     {%- else -%}
 
